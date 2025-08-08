0ff75a60cf8c470c65efcf32ea0f9eec38a817ba
Alejandro Valle <alejandro_valle99@hotmail.com>
2025-08-08 07:33:37 -0600

    env variables

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ app.post("/new", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on http://localhost:3000`);
 });
